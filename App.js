7b5a4c3cecb7c5e781fd81d30fd30c6458bf52c3
77046302+ChhuongLe@users.noreply.github.com <chhuongcle@gmail.com>
2021-10-21 16:48:40 -0700

    Add home screen for user and indicated todo for future dev

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import TodoList from './components/TodoList';
+import Home from './components/Home';
 import Task from './components/Task';
 
 // This allows us the usage of having multiple screens in an app
@@ -11,6 +12,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="homePage" component={Home} />
         <Stack.Screen name="todoList" component={TodoList} />
       </Stack.Navigator>
     </NavigationContainer>
