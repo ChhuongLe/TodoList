100a33bc979975e0618dbd700b294c1fe9e1a0ee
77046302+ChhuongLe@users.noreply.github.com <chhuongcle@gmail.com>
2021-10-21 16:53:14 -0700

    Add icons using expo vector icons library

--- components/Home.js
+++ components/Home.js
@@ -10,6 +10,7 @@ import {
   Keyboard,
   AsyncStorage
 } from 'react-native';
+import { Ionicons} from '@expo/vector-icons';
 // This will be the first screen the user will see
 // Allows them to make containers of Todos for different tasks
 const Home = () => {
@@ -21,6 +22,12 @@ const Home = () => {
           <Text style={styles.itemTitle}>List 1</Text>
         </View>
         <View style={{flexDirection:"row"}}>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="options-outline" size={24} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="trash-outline" size={24} color="white" />
+          </TouchableOpacity>
         </View>
       </TouchableOpacity>
     </View>
@@ -42,6 +49,7 @@ const styles = StyleSheet.create({
     marginVertical: 10,
     marginHorizontal: 10,
     alignItems: "center",
+    justifyContent: "space-between",
   },
   itemTitle: {
     fontSize: 24,
